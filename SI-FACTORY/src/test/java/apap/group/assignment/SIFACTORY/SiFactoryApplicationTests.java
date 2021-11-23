4d3ad796ea5ab0cb6e05cee0fde309934616afb1
Hamzah Arrayhan <hamzahrayhan@gmail.com>
2021-11-23 23:17:53 +0700

    first commit

---
+++ SI-FACTORY/src/test/java/apap/group/assignment/SIFACTORY/SiFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.group.assignment.SIFACTORY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
