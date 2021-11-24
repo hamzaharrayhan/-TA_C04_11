a48a1f79db85eed8c68ffeb44b31ab08d84d7247
pllips <61380954+pllips@users.noreply.github.com>
2021-11-25 01:47:57 +0700

    feat: LOGIN SCRATCH (80%) (#2)
    merged by phillips

---
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/repository/ProduksiDB.java
@@ -0,0 +1,7 @@
+package apap.group.assignment.SIFACTORY.repository;
+
+import apap.group.assignment.SIFACTORY.model.ProduksiModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProduksiDB extends JpaRepository<ProduksiModel, Long> {
+}
---
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/repository/RequestUpdateItemDB.java
@@ -0,0 +1,7 @@
+package apap.group.assignment.SIFACTORY.repository;
+
+import apap.group.assignment.SIFACTORY.model.RequestUpdateItemModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestUpdateItemDB extends JpaRepository<RequestUpdateItemModel, Long> {
+}
