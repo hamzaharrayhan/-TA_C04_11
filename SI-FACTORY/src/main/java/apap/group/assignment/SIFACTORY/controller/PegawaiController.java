660afe698415a7a2b0af0c53e6e7b44f395a66dd
Annisa Amalia S <amalias.annisa@gmail.com>
2021-11-26 19:36:49 +0700

    uncomment security dependency on pom.xml

--- SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/controller/PegawaiController.java
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/controller/PegawaiController.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.sound.midi.Soundbank;
 import java.lang.reflect.Array;
 import java.time.LocalTime;
 import java.util.List;
