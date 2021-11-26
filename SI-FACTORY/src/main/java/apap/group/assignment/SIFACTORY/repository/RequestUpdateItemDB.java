3dbf4fecf631b7d23842391dc572facdf8a59f24
Hamzah Arrayhan <hamzahrayhan@gmail.com>
2021-11-26 08:27:57 +0700

    home page

--- SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/repository/RequestUpdateItemDB.java
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/repository/RequestUpdateItemDB.java
@@ -4,4 +4,5 @@ import apap.group.assignment.SIFACTORY.model.RequestUpdateItemModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequestUpdateItemDB extends JpaRepository<RequestUpdateItemModel, Long> {
+    RequestUpdateItemModel findByIdRequestUpdateItem(Integer id);
 }
