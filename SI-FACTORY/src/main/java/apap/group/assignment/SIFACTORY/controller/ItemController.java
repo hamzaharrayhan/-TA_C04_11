ff43566df61ead2c7c8a0333749b4cde3a4ca0f7
faizah0212 <90332436+faizah0212@users.noreply.github.com>
2021-12-07 18:34:19 +0700

    Faizah (#8)
    * feat: LOGOUT (100%)
    
    * add navbar to home
    
    Co-authored-by: unknown <faizahs0212@gmail.com>

---
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/controller/ItemController.java
@@ -0,0 +1,43 @@
+package apap.group.assignment.SIFACTORY.controller;
+
+import apap.group.assignment.SIFACTORY.rest.ItemModel;
+import apap.group.assignment.SIFACTORY.service.ItemRestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
+@RequestMapping("/item")
+public class ItemController {
+
+    @Autowired
+    private ItemRestService itemRestService;
+
+    @GetMapping("/viewall")
+    public String listItem(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String role = auth.getAuthorities().toString().toString().replace("[", "").replace("]", "");
+        HashMap<String, List<ItemModel>> itemHashMap = itemRestService.retrieveListItem();
+
+        System.out.println("map: " + itemHashMap);
+        System.out.println("role: " + role);
+
+        for (String name: itemHashMap.keySet()) {
+            String key = name.toString();
+            List value = itemHashMap.get(name);
+            System.out.println(key + " " + value.toString());
+        }
+
+        model.addAttribute("role", role);
+        model.addAttribute("itemHashMap", itemHashMap);
+
+        return "viewall-item";
+    }
+}
---
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/service/ItemRestServiceImpl.java
@@ -0,0 +1,55 @@
+package apap.group.assignment.SIFACTORY.service;
+
+import apap.group.assignment.SIFACTORY.rest.ItemModel;
+import apap.group.assignment.SIFACTORY.rest.Setting;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@Transactional
+public class ItemRestServiceImpl implements ItemRestService {
+    private final WebClient webClient;
+
+    public ItemRestServiceImpl(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(Setting.itemUrl).build();
+    }
+
+    @Override
+    public HashMap<String, List<ItemModel>> retrieveListItem() {
+        HashMap<String, Object> response = this.webClient
+                .get()
+                .uri("/api/item")
+                .retrieve()
+                .bodyToMono(HashMap.class)
+                .block();
+
+        List<HashMap> item = (List<HashMap>) response.get("result");
+        HashMap<String, List<ItemModel>> result = new HashMap<>();
+
+        for (int i = 0; i < item.size(); i++) {
+            ItemModel tempItem = new ItemModel();
+            HashMap itemJson = item.get(i);
+            tempItem.setUuid((String) itemJson.get("uuid"));
+            tempItem.setNama((String) itemJson.get("nama"));
+            tempItem.setHarga((Integer) itemJson.get("harga"));
+            tempItem.setStok((Integer) itemJson.get("stok"));
+            tempItem.setKategori((String) itemJson.get("kategori"));
+
+            String tempItemKategori = tempItem.getKategori();
+            if (result.containsKey(tempItemKategori)) {
+                result.get(tempItemKategori).add(tempItem);
+            } else {
+                List<ItemModel> listItem = new ArrayList<ItemModel>();
+                result.put(tempItemKategori, listItem);
+                result.get(tempItemKategori).add(tempItem);
+            }
+        }
+        return result;
+    }
+}
+
