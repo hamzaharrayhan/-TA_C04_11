19a47731a2db17353234f0d00ce81e5ff78e0e64
Hamzah Arrayhan <hamzahrayhan@gmail.com>
2021-12-14 20:33:35 +0700

    deploy-no-auth-admin8)

--- SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/service/RequestUpdateItemRestService.java
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/service/RequestUpdateItemRestService.java
@@ -5,12 +5,15 @@ import apap.group.assignment.SIFACTORY.model.PegawaiModel;
 import apap.group.assignment.SIFACTORY.model.RequestUpdateItemModel;
 import apap.group.assignment.SIFACTORY.rest.ItemDetail;
 import apap.group.assignment.SIFACTORY.rest.ItemModel;
+import apap.group.assignment.SIFACTORY.rest.RequestUpdateItemDTO;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 //import netscape.javascript.JSObject;
 import org.springframework.util.MultiValueMap;
 import reactor.core.publisher.Mono;
 
 public interface RequestUpdateItemRestService {
-    RequestUpdateItemModel addRequestUpdateItem(RequestUpdateItemModel item);
-//    RequestUpdateItemModel getRequestUpdateItemByIdRequestUpdateItem(Long idRequestUpdateItem);
+    RequestUpdateItemModel addRequestUpdateItem(RequestUpdateItemDTO item);
+
+    //    RequestUpdateItemModel getRequestUpdateItemByIdRequestUpdateItem(Long idRequestUpdateItem);
     ItemModel updateReqItem(ItemModel item, Integer jumlahStokDitambahkan, MesinModel mesin, String username, Long idRequestUpdateItem);
+}
