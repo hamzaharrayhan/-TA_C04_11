c9d8afe016104d9c56994f516e515c844291d9db
unknown <faizahs0212@gmail.com>
2021-12-14 22:08:05 +0700

    fix minor

--- SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/service/RequestUpdateItemRestService.java
+++ SI-FACTORY/src/main/java/apap/group/assignment/SIFACTORY/service/RequestUpdateItemRestService.java
@@ -1,16 +1,14 @@
 package apap.group.assignment.SIFACTORY.service;
 
 import apap.group.assignment.SIFACTORY.model.MesinModel;
-import apap.group.assignment.SIFACTORY.model.PegawaiModel;
 import apap.group.assignment.SIFACTORY.model.RequestUpdateItemModel;
-import apap.group.assignment.SIFACTORY.rest.ItemDetail;
 import apap.group.assignment.SIFACTORY.rest.ItemModel;
-import com.fasterxml.jackson.databind.util.JSONPObject;
+
 //import netscape.javascript.JSObject;
-import org.springframework.util.MultiValueMap;
-import reactor.core.publisher.Mono;
 
 public interface RequestUpdateItemRestService {
     RequestUpdateItemModel addRequestUpdateItem(RequestUpdateItemModel item);
-//    RequestUpdateItemModel getRequestUpdateItemByIdRequestUpdateItem(Long idRequestUpdateItem);
+
+    //    RequestUpdateItemModel getRequestUpdateItemByIdRequestUpdateItem(Long idRequestUpdateItem);
     ItemModel updateReqItem(ItemModel item, Integer jumlahStokDitambahkan, MesinModel mesin, String username, Long idRequestUpdateItem);
+}
